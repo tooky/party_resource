b225ab527acb55d03f95a22bb5d38bc559492864
Steve Tooke and Tristan Harris <dev+tooky+tristanharris@edendevelopment.co.uk>
2010-05-17 14:55:50 +0100

    Add support for different HTTP Verbs
    Only allow one verb at a time

--- lib/party_resource/route.rb
+++ lib/party_resource/route.rb
@@ -1,15 +1,24 @@
 module PartyResource
   class Route
 
+    VERBS = [:get, :post, :put, :delete]
+
     def initialize(options = {})
-      options[:verb] = :get
-      options[:path] = options.delete(options[:verb])
-      @options = options
+      @options = transform_options(options)
     end
 
     def call(context, *args)
       path = Path.new(@options[:path], context)
       :foo
+    end
+
+    private
+    def transform_options(options)
+      verbs = options.keys & VERBS
+      raise ArgumentError.new("Must define only one verb (#{verbs.inspect} defined)") unless verbs.size == 1
+      options[:verb] = verbs.first
+      options[:path] = options.delete(options[:verb])
+      options
     end
   end
 end
--- spec/route_spec.rb
+++ spec/route_spec.rb
@@ -3,16 +3,37 @@ require 'spec_helper'
 describe PartyResource::Route do
   subject { PartyResource::Route.new options }
   let(:path) { mock(:path) }
+  let(:object) { mock(:object) }
+
+  def self.it_builds_the_request_path
+    it "builds the request path" do
+      PartyResource::Path.should_receive(:new).with(path, object)
+      subject.call(object)
+    end
+  end
 
   context "for a get request" do
     let(:options) { { :get => path } }
 
     describe ".call" do
-      let(:object) { mock(:object) }
+      it_builds_the_request_path
+    end
+  end
 
-      it "builds the request path" do
-        PartyResource::Path.should_receive(:new).with(path, object)
-        subject.call(object)
+  context "for a post request" do
+    let(:options) { { :post => path } }
+
+    describe ".call" do
+      it_builds_the_request_path
+    end
+  end
+
+  context 'with more than one verb passed' do
+    let(:options) { { :post => path, :get => path } }
+
+    describe ".call" do
+      it "raises an argument error" do
+        lambda{ subject.call(object) }.should raise_error(ArgumentError)
       end
     end
   end
